8b401c2936d32c501347e0c7e8eb59b109806406
Sadie James <120068254+SadieJames3@users.noreply.github.com>
2023-04-03 18:03:22 -0400

    Add files via upload
    Initial commit. Not completed. Does not yet work. Will continue.

---
+++ random.js
@@ -0,0 +1,24 @@
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '5ecf89a01amsh71a96459b0fe03dp183082jsn27bd32241c42',
+// 		'X-RapidAPI-Host': 'random-recipes.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://random-recipes.p.rapidapi.com/ai-quotes/1', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+const 
+const theRecipe = document.getElementById(getRecipe)
+
+
+// class="results">
+//             <div id="recipe-result" class="result">
+//                 <p>Result Appears Here</p>
+//             </div>
+//         </div>
+//         <div class="button">
+//             <button id="getRecipe">Try It!</button>
